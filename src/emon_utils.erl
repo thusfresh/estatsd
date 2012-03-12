e113e3467e59923b67ee3ecc4a4187ed3896544d
Enrique <Enrique.PazPerez@spilgames.com>
2012-03-12 11:19:40 +0100

    NEW emon_utils:get_key_string/3
    * Added 'environment' key to configuration.
    * Added missing doc to emon_uils function
    * overview.edoc added

--- src/emon_utils.erl
+++ src/emon_utils.erl
@@ -1,6 +1,6 @@
 -module(emon_utils).
 
--export([statsnode/0, get_key_string/4]).
+-export([statsnode/0, get_key_string/3, get_key_string/4]).
 
 %%--------------------------------------------------------------------
 %% @doc Returns the nodename with the '@' replaced by '_'
@@ -13,16 +13,26 @@ statsnode() ->
             ($@) -> $_;
             (Other) -> Other
         end, N).
+%%--------------------------------------------------------------------
+%% @doc Returns a valid keyname according to spilgames naming rules for monitoring. The 'Environment' part of e key will
+%% be read from configuration and defaulted to 'development'
+-spec get_key_string(string(), string(), [string()]) -> string().
+%% @end
+%%--------------------------------------------------------------------
+get_key_string(Team, Application, MetricChain) when MetricChain /= [] ->
+    Environment=elibs_application:get_env(erlang_monitoring, environment, "development"),
+    get_key_string(Environment, Team, Application, MetricChain).
+
 
 %%--------------------------------------------------------------------
-%% @doc
+%% @doc Returns a valid keyname according to spilgames naming rules for monitoring.
 -spec get_key_string(string(), string(), string(), [string()]) -> string().
 %% @end
 %%--------------------------------------------------------------------
 get_key_string(Environment, Team, Application, MetricChain) when MetricChain /= [] ->
     MetricName=string:join([elibs_types:to_type(str, M, "unknown") || M <- MetricChain], "."),
     elibs_string:format("~s.~s.~s.~s.~s",
-        [elibs_types:to_type(str, Environment, "unknown"), elibs_types:to_type(str, Team, "unknown"),
+        [elibs_types:to_type(str,Environment, "unknown"), elibs_types:to_type(str, Team, "unknown"),
             elibs_types:to_type(str, Application, "unknown"), MetricName, statsnode()]
     ).
 
--- test/emon_utils_eunit.erl
+++ test/emon_utils_eunit.erl
@@ -14,8 +14,28 @@ statsnode_test_() ->
 
 get_key_string_test_() ->
     Node=emon_utils:statsnode(),
-    Expected=elibs_string:format("env.team.app.metric.submetric1.submetric2.~s", [Node]),
+    Expected1=elibs_string:format("development.team.app.metric.submetric1.submetric2.~s", [Node]),
+    Expected2=elibs_string:format("myenv.team.app.metric.submetric1.submetric2.~s", [Node]),
     [
-        ?_assertEqual(Expected, emon_utils:get_key_string("env", "team", "app", ["metric", "submetric1", "submetric2"]))
+        ?_assertEqual(Expected1, emon_utils:get_key_string("team", "app", ["metric", "submetric1", "submetric2"])),
+        ?_assertEqual(Expected2, emon_utils:get_key_string("myenv", "team", "app", ["metric", "submetric1", "submetric2"]))
     ].
+
+get_key_string_with_env_test_() ->
+    {setup,
+        fun () ->
+                {ok, F} = elibs_application:set_env(erlang_monitoring, environment, "myenv"),
+                [F]
+        end,
+        fun (Cleanup) ->
+                lists:foreach(fun (F) -> F() end, Cleanup)
+        end,
+        fun (_) ->
+                Node=emon_utils:statsnode(),
+                Expected=elibs_string:format("myenv.team.app.metric.submetric1.submetric2.~s", [Node]),
+                [
+                    ?_assertEqual(Expected, emon_utils:get_key_string("team", "app", ["metric", "submetric1", "submetric2"]))
+                ]
+        end
+    }.
 
